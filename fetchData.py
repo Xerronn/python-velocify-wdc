f023c668ab05540ed124c5fc31d66bf66cce5ab6
Zack <theXerronn@gmail.com>
2021-05-11 23:10:17 -0400

    Added the lead fields table

--- fetchData.py
+++ fetchData.py
@@ -6,12 +6,14 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 from datetime import datetime
 import dateutil.relativedelta
+import logging as log
 
 #for testing
 import time
 
-#generate a google service account with json keys, add them to the bucket add data permission group
+#generate a google service account with json keys, add them to the bucket's add data permission group
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keys.json"
+log.basicConfig(level=log.DEBUG)
 
 with open('credentials.json') as f:
     credentials = json.load(f)
@@ -53,42 +55,45 @@ yearAgo = today + dateutil.relativedelta.relativedelta(years=-1)
 #paginate through one month intervals until reaching one year
 for i in range(12):
     leadsDF = pd.DataFrame({"Id":pd.Series([], dtype=int)})
-    print(f"Getting month {i} data...")
+    
     
     attempt = 0
     while(True):
         try:
+            log.info(f"Getting month {i} data...")
             start_time_query = time.time()
+
             query = requests.get(f"https://service.prod.velocify.com/ClientService.asmx/getLeads?username={credentials['username']}&password={credentials['password']}&from="
             f"{(yearAgo + dateutil.relativedelta.relativedelta(months=i, days=-1)).strftime('%m/%d/%Y')}&to={(yearAgo + dateutil.relativedelta.relativedelta(months=i+1)).strftime('%m/%d/%Y')}")
-            print(f"--- {time.time() - start_time_query} seconds for query on month {i} ---")
+
+            log.info(f"--- {time.time() - start_time_query} seconds for query on month {i} ---")
             break
         except KeyboardInterrupt:
             raise KeyboardInterrupt
         except:
-            print("Connection error, retrying...")
+            log.warning("Connection error, retrying...")
             if attempt > 3:
                 raise FailedToConnectError
             else:
                 attempt += 1
     
-    start_time_parsing = time.time()
     data = ET.fromstring(query.content)
-    print(f"--- {time.time() - start_time_parsing} seconds for parsing on month {i} ---")
-
+    
     start_time_processing = time.time()
-    rows= {}
+    leadAttribs = {}
+    leadFields = {}
     for lead in data:
+        #start of lead attributes table
         for key in lead.attrib.keys():
-            rows.setdefault(key,[]).append(lead.attrib[key])
+            leadAttribs.setdefault(key,[]).append(lead.attrib[key])
         #give one version of the ID that is int and one that is string
-        rows.setdefault("EncompassId",[]).append(lead.attrib["Id"])
+        leadAttribs.setdefault("EncompassId", []).append(lead.attrib["Id"])
 
         try:
             #campaign table
             campaign = lead.find("Campaign")
             for key in campaign.attrib.keys():
-                rows.setdefault(key,[]).append(campaign.attrib[key])
+                leadAttribs.setdefault(key, []).append(campaign.attrib[key])
         except:
             pass
         
@@ -96,7 +101,7 @@ for i in range(12):
             #status table
             status = lead.find("Status")
             for key in status.attrib.keys():
-                rows.setdefault(key,[]).append(status.attrib[key])
+                leadAttribs.setdefault(key, []).append(status.attrib[key])
         except:
             pass
         
@@ -104,22 +109,39 @@ for i in range(12):
             #agent table
             agent = lead.find("Agent")
             for key in agent.attrib.keys():
-                rows.setdefault(key,[]).append(agent.attrib[key])
+                leadAttribs.setdefault(key, []).append(agent.attrib[key])
+        except:
+            pass
+        #End of lead attributes table
+
+        #start of lead fields table
+        leadFields.setdefault("LeadId",[]).append(lead.attrib["Id"])
+        try:
+            for field in lead.iter('Field'):
+                leadFields.setdefault("FieldId", []).append(field.attrib["FieldId"])
+                leadFields.setdefault(field.attrib["FieldTitle"], []).append(field.attrib["Value"])
         except:
             pass
 
+    log.info(f"--- {time.time() - start_time_processing} seconds for processing on month {i} ---")        
+    
     #dataframes are slow but they are fine for this instance.
-    start_time_df = time.time()
-    leadsDF = pd.DataFrame.from_dict(rows,orient='index').transpose()
-    print(f"--- {time.time() - start_time_df} seconds for df append on month {i} ---")
+    start_time_df1 = time.time()
+    leadAttribsDF = pd.DataFrame.from_dict(leadAttribs,orient='index').transpose()
+    log.info(f"--- {time.time() - start_time_df1} seconds for df creation on month {i} ---")   
+    start_time_df2 = time.time()
+    leadFieldsDF = pd.DataFrame.from_dict(leadFields,orient='index').transpose()
+    log.info(f"--- {time.time() - start_time_df2} seconds for df creation on month {i} ---")     
     
     if i == 0:
-        leadsDF.to_csv("LeadsData.csv")
+        leadAttribsDF.to_csv("LeadAttributes.csv")
+        leadFieldsDF.to_csv("LeadFields.csv")
     else:
-        leadsDF.to_csv("LeadsData.csv", mode='a', header=False)
+        leadAttribsDF.to_csv("LeadAttributes.csv", mode='a', header=False)
+        leadFieldsDF.to_csv("LeadFields.csv", mode='a', header=False)
 
-    print(f"--- {time.time() - start_time_processing} seconds for processing on month {i} ---")
-
-upload_blob("angel_oak", "LeadsData.csv", f"LeadsTest{datetime.now().strftime('%d-%m-%Y_%H:%M:%S')}.csv")
+uploadTime = datetime.now().strftime('%d-%m-%Y_%H:%M:%S')
+upload_blob("angel_oak", "LeadAttributes.csv", f"{uploadTime}/LeadAttributes.csv")
+upload_blob("angel_oak", "LeadFields.csv", f"{uploadTime}/LeadFields.csv")
 
 print("--- %s seconds ---" % (time.time() - start_time))
